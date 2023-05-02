90f26e270c007be35f7a7c114d5adb5b25cc0ebb
Alex <kozlov.alex.dev@gmail.com>
2023-05-02 17:36:38 +0300

    Refactoring repo and useCases to implement LiveData.

--- app/src/main/java/com/countlesswrongs/myshoppinglist/domain/usecase/GetShopItemListUseCase.kt
+++ app/src/main/java/com/countlesswrongs/myshoppinglist/domain/usecase/GetShopItemListUseCase.kt
@@ -1,11 +1,12 @@
 package com.countlesswrongs.myshoppinglist.domain.usecase
 
+import androidx.lifecycle.LiveData
 import com.countlesswrongs.myshoppinglist.domain.ShopListRepository
 import com.countlesswrongs.myshoppinglist.domain.model.ShopItem
 
 class GetShopItemListUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItemList(): List<ShopItem> {
+    fun getShopItemList(): LiveData<List<ShopItem>> {
         return shopListRepository.getShopItemList()
     }
 }
