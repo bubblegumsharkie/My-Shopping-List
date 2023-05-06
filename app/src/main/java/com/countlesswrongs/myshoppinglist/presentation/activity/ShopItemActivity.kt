45cdde8f452e062671e4c369261ed6ad45a85631
Alex <kozlov.alex.dev@gmail.com>
2023-05-06 14:59:50 +0300

    Activity impl start

--- app/src/main/java/com/countlesswrongs/myshoppinglist/presentation/activity/ShopItemActivity.kt
+++ app/src/main/java/com/countlesswrongs/myshoppinglist/presentation/activity/ShopItemActivity.kt
@@ -3,17 +3,88 @@ package com.countlesswrongs.myshoppinglist.presentation.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.countlesswrongs.myshoppinglist.R
+import com.countlesswrongs.myshoppinglist.domain.model.ShopItem
+import com.countlesswrongs.myshoppinglist.presentation.viewmodel.ShopItemViewModel
+import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var textInputLayoutName: TextInputLayout
+    private lateinit var textInputLayoutAmount: TextInputLayout
+    private lateinit var editTextName: EditText
+    private lateinit var editTextAmount: EditText
+    private lateinit var buttonSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("ShopItemActivity", mode.toString())
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener {
+            val name = editTextName.text.toString()
+            val amount = editTextAmount.text.toString()
+            viewModel.addShopItem(name, amount)
+            finish()
+        }
+        TODO("Implement error messages")
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemId)
+        viewModel.currentShopItem.observe(this) {
+            editTextName.setText(it.name)
+            editTextAmount.setText(it.amount.toString())
+        }
+        buttonSave.setOnClickListener {
+            val name = editTextName.text.toString()
+            val amount = editTextAmount.text.toString()
+            viewModel.editShopItem(name, amount)
+            finish()
+        }
+        TODO("Implement error messages")
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Screen mode extra was not found")
+        }
+        val stringExtra = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (stringExtra != MODE_EDIT && stringExtra != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode: $stringExtra")
+        }
+        screenMode = stringExtra
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Shop item id was not found")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    private fun initViews() {
+        textInputLayoutName = findViewById(R.id.textInputLayoutName)
+        textInputLayoutAmount = findViewById(R.id.textInputLayoutAmount)
+        editTextName = findViewById(R.id.editTextName)
+        editTextAmount = findViewById(R.id.editTextAmount)
+        buttonSave = findViewById(R.id.buttonSave)
     }
 
     companion object {
@@ -22,6 +93,7 @@ class ShopItemActivity : AppCompatActivity() {
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
 
         fun newIntentAddShopItem(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
