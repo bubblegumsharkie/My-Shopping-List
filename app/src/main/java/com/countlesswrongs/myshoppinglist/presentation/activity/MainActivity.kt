3f375ee230b81ddfb5956355a8a57e8f620fc6db
Alex <kozlov.alex.dev@gmail.com>
2023-06-07 12:36:46 +0300

    Deletion via contentResolver

--- app/src/main/java/com/countlesswrongs/myshoppinglist/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/countlesswrongs/myshoppinglist/presentation/activity/MainActivity.kt
@@ -138,7 +138,14 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val item = shopListAdapter.currentList[viewHolder.adapterPosition]
-                viewModel.deleteShopItem(item)
+//                viewModel.deleteShopItem(item)
+                thread {
+                    contentResolver.delete(
+                        Uri.parse("content://com.countlesswrongs.myshoppinglist/shop_items"),
+                        null,
+                        arrayOf(item.id.toString())
+                    )
+                }
             }
         }
         val itemTouchHelper = ItemTouchHelper(callback)
